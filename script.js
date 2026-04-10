76032cf2e26de2e1d6135ee56a0c773f2a4b42ff
Сячнева Валентина Алексеевна <240797@edu.fa.ru>
2026-04-10 09:56:21 +0300

    Added basic UI and functionality for adding tasks

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function addTask() {
+  const input = document.getElementById('newTaskInput');
+  if(input.value.trim()) {
+    const taskList = document.getElementById('taskList');
+    const newItem = document.createElement('li');
+    newItem.textContent = input.value;
+    taskList.appendChild(newItem);
+    input.value = '';
+  }
+}
